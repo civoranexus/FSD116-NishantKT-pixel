74591409132a9a7761bebdc40f384b192889b7df
NishantKT-pixel <kumarnthakur9696@gmail.com>
2026-01-18 19:18:20 +0530

    Added request logging and centralised error handling middleware

--- backened/src/app.js
+++ backened/src/app.js
@@ -1,8 +1,12 @@
+const errorHandler=require("./middleware/errorHandler");
 const express=require("express");
+const logger=require("./middleware/logger");
 const app=express();
 
 const plantRoutes=require("./routes/plantRoutes");
 
 app.use(express.json());
+app.use(logger);
 app.use("/plants",plantRoutes);
+app.use(errorHandler);
 module.exports=app;
